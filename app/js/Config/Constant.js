5b88a2ec79c80e7d19e8facaedb1156ed2ba5b97
moises <mvelasquez@pictoric.com>
2017-07-04 10:32:20 -0500

    service constant and value

---
+++ app/js/Config/Constant.js
@@ -0,0 +1,6 @@
+angular.module('miApp')
+.constant('CSRF_TOKEN', '94a08da1fecbb6e8b46990538c7b50b2')
+.constant('API_TOKEN', {
+	_public: 'a2d10a3211b415832791a6bc6031f9ab',
+	_secret: '5ebe2294ecd0e0f08eab7690d2a6ee69'
+});
---
+++ app/js/Config/Values.js
@@ -0,0 +1,2 @@
+angular.module('miApp')
+.values('API_URL', 'api.example.com/v1');
