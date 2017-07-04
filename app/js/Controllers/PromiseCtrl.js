8de63603979e21ac6b924ed204a2d47fc437b1bb
moises <mvelasquez@pictoric.com>
2017-07-04 12:27:46 -0500

    varias promesas a la vez

--- app/js/Controllers/PromiseCtrl.js
+++ app/js/Controllers/PromiseCtrl.js
@@ -73,4 +73,34 @@ angular.module('miApp')
 	},function(notify){
 		console.log(notify);
 	});
+}])
+.controller('AppCtrl',['$q', function($q){
+	var promesa1 = $q.defer();
+	var promesa2 = $q.defer();
+	var promesa3 = $q.defer();
+
+	promesa1.promise.then(completado);
+	promesa2.promise.then(completado);
+	promesa3.promise.then(completado);
+
+	function completado(data){
+		console.log(data);
+	}
+
+	setTimeout(function(){
+		promesa1.resolve('Promesa #1 resuelta');
+	}, Math.random() * 1000);
+
+	setTimeout(function(){
+		promesa2.resolve('Promesa #2 resuelta');
+	}, Math.random() * 1000);
+
+	setTimeout(function(){
+		promesa3.resolve('Promesa #3 resuelta');
+	}, Math.random() * 1000);
+
+	var todas = $q.all([promesa1.promise, promesa2.promise, promesa3.promise]);
+	todas.then(function(data){
+		console.log(data);
+	});
 }]);
