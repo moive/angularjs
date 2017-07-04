55d5832d0c3d6a5ae51ea7303b6b271f0641a901
moises <mvelasquez@pictoric.com>
2017-07-04 13:49:12 -0500

    constructor de promesas

--- app/js/Controllers/PromiseCtrl.js
+++ app/js/Controllers/PromiseCtrl.js
@@ -74,33 +74,26 @@ angular.module('miApp')
 		console.log(notify);
 	});
 }])
-.controller('AppCtrl',['$q', function($q){
-	var promesa1 = $q.defer();
-	var promesa2 = $q.defer();
-	var promesa3 = $q.defer();
-
-	promesa1.promise.then(completado);
-	promesa2.promise.then(completado);
-	promesa3.promise.then(completado);
-
-	function completado(data){
-		console.log(data);
+.controller('AppCtrl',['$scope', '$q', function($scope, $q){
+	function tarea(comprobar){
+		var dfd = $q.defer();
+		setTimeout(function(){
+			if (!comprobar){
+				dfd.resolve('Promesa resuelta');
+			}else{
+				dfd.reject('Promesa rechazada');
+			}
+		},1000);
+		return dfd.promise;
 	}
 
-	setTimeout(function(){
-		promesa1.resolve('Promesa #1 resuelta');
-	}, Math.random() * 1000);
+	$scope.accion = ejecutar;
 
-	setTimeout(function(){
-		promesa2.resolve('Promesa #2 resuelta');
-	}, Math.random() * 1000);
-
-	setTimeout(function(){
-		promesa3.resolve('Promesa #3 resuelta');
-	}, Math.random() * 1000);
-
-	var todas = $q.all([promesa1.promise, promesa2.promise, promesa3.promise]);
-	todas.then(function(data){
-		console.log(data);
-	});
+	function ejecutar(comprobar){
+		tarea(comprobar).then(function(data){
+			$scope.resuelta = data;
+		}, function(error){
+			$scope.rechazada = error;
+		});
+	}
 }]);
