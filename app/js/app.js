78ecd1022f5186cdae4b7bd9596e2948ca23a139
moises <mvelasquez@pictoric.com>
2017-07-06 17:19:51 -0500

    quitar anotaciones innecesarias

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,12 @@
 'use-strict';
 
 angular.module('miApp', [])
-.config(['$qProvider','$logProvider', function ($qProvider,$logProvider) {
+.config(['$qProvider','$logProvider', '$compileProvider', function ($qProvider,$logProvider, $compileProvider) {
             $qProvider.errorOnUnhandledRejections(false);
             $logProvider.debugEnabled(false);
+
+            //quita anotaciones innecesarias
+            $compileProvider.debugInfoEnabled(false);
         }])
 /*.run(['$anchorScroll', function($anchorScroll){
 	$anchorScroll.yOffset = 50;
