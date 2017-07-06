33e4aad07d8962e57c33d39387101d5caaa35e1f
moises <mvelasquez@pictoric.com>
2017-07-06 15:33:52 -0500

    timeout

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,9 @@
 'use-strict';
 
 angular.module('miApp', [])
+.config(['$qProvider', function ($qProvider) {
+            $qProvider.errorOnUnhandledRejections(false);
+        }])
 /*.run(['$anchorScroll', function($anchorScroll){
 	$anchorScroll.yOffset = 50;
 }]);*/
