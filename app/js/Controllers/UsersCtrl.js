f1952496afe1798306ea35aca64595ae1f1df0b7
moises <mvelasquez@pictoric.com>
2017-07-11 10:45:44 -0500

    http.get

--- app/js/Controllers/UsersCtrl.js
+++ app/js/Controllers/UsersCtrl.js
@@ -1,12 +1,8 @@
 angular.module("miApp")
 .controller('UsersCtrl',['$scope', '$log', '$http', function($scope, $log, $http){
-	var usuarios = $http({
-		method: 'GET',
-		url: '../../json/usuarios.json'
-	}).then(function(response, status){
+	$http.get('./json/usuarios.json').then(function(response){
 		$scope.usuarios = response.data;
-		$log.log(response);
-	}, function(response){
+	}, function(error){
 		$log.log(response.statusText);
-	})
+	});
 }]);
