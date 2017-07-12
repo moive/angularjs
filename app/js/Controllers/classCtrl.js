8ef00ca148557a6e12ffc2aa854d81e023ab40a6
moises <mvelasquez@pictoric.com>
2017-07-12 14:41:03 -0500

    expresion evalua objeto

--- app/js/Controllers/classCtrl.js
+++ app/js/Controllers/classCtrl.js
@@ -1,7 +1,4 @@
 angular.module('miApp')
 .controller("classCtrl",['$scope', function($scope){
-	var classes = ['flotar', 'fondoRojo', 'borderRedondeados'];
-	$scope.generateClass = function(){
-		return classes;
-	};
+	//
 }]);
