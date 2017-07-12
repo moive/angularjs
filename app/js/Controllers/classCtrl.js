e608a74dbb9c6c0531ac8ff02e147f505af4dd20
moises <mvelasquez@pictoric.com>
2017-07-12 17:19:03 -0500

    styles

--- app/js/Controllers/classCtrl.js
+++ app/js/Controllers/classCtrl.js
@@ -1,4 +1,11 @@
 angular.module('miApp')
 .controller("classCtrl",['$scope', function($scope){
-	//
+	var styles = {
+		'font-weight': 'bold',
+		'color': '#eee'
+	};
+	
+	$scope.styles = styles;
+	
+	$scope.etiquetas = ['Actualidad', 'Finanzas', 'Tecnología'];
 }]);
