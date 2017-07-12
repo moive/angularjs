7539c3c22b230708487750ad80848d62767672ca
moises <mvelasquez@pictoric.com>
2017-07-12 14:09:11 -0500

    expresion evaluada a una cadena de texto

---
+++ app/js/Controllers/classCtrl.js
@@ -0,0 +1,7 @@
+angular.module('miApp')
+.controller("classCtrl",['$scope', function($scope){
+	var classes = ['flotar', 'fondoRojo', 'borderRedondeados'];
+	$scope.generateClass = function(){
+		return classes.join(' ');
+	};
+}]);
