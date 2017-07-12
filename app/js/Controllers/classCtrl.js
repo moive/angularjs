dcbb47c4e2e9065b95afd6875feec44aa2fbcb39
moises <mvelasquez@pictoric.com>
2017-07-12 14:10:49 -0500

    expresion es evaluada a un arreglo

--- app/js/Controllers/classCtrl.js
+++ app/js/Controllers/classCtrl.js
@@ -2,6 +2,6 @@ angular.module('miApp')
 .controller("classCtrl",['$scope', function($scope){
 	var classes = ['flotar', 'fondoRojo', 'borderRedondeados'];
 	$scope.generateClass = function(){
-		return classes.join(' ');
+		return classes;
 	};
 }]);
