0cca53e6831ae403c32f12c56012654cc39ab913
moises <mvelasquez@pictoric.com>
2017-07-13 12:22:19 -0500

    variables ng-repeat

---
+++ app/js/Controllers/directivasCtrl.js
@@ -0,0 +1,15 @@
+angular.module('miApp')
+.controller('directivasCtrl',['$scope', function($scope){
+	var cds = [
+		'Songs of Innocence',
+		'Forget the World',
+		'Unlocked',
+		'True',
+		'The New Daylight',
+		'Lovers on the Sun',
+		'Talking Dreams',
+		'Trouble in paradise'
+	];
+	
+	$scope.cds = cds;
+}]);
