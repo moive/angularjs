e285d9f39ddaba2a77c9ce39e691494abfadfb1f
moises <mvelasquez@pictoric.com>
2017-07-13 14:59:35 -0500

    events key and mouse

--- app/js/Controllers/directivasCtrl.js
+++ app/js/Controllers/directivasCtrl.js
@@ -1,5 +1,5 @@
 angular.module('miApp')
-.controller('directivasCtrl',['$scope', function($scope){
+.controller('directivasCtrl',['$scope', '$log', function($scope, $log){
 	var cds = [
 		'Songs of Innocence',
 		'Forget the World',
@@ -14,4 +14,47 @@ angular.module('miApp')
 	$scope.cds = cds;
 	
 	$scope.mostrar = true;
+	
+	$scope.keydown = function(e){
+		$log.log('Key down - Key Code: ' + e.keyCode, 'altKey: ' + e.altKey);
+	};
+	
+	$scope.keypress = function(e){
+		$log.log('Key press - Key Code: ' + e.keyCode, 'altKey: ' + e.altKey);
+	};
+	
+	$scope.keyup = function(e){
+		$log.log('Key up - Key Code: ' + e.keyCode, 'altKey: ' + e.altKey);
+	};
+	
+	$scope.style = {
+		'border': '2px solid black',
+		'width': '200px',
+		'height': '200px',
+		'background-color': '#56A5F3'
+	};
+	
+	$scope.down = function(e){
+		$log.log('Ejecutado el evento Mousedown');
+	};
+	
+	$scope.up = function(e){
+		$log.log('Ejecutado el evento Mouseup');
+	};
+	
+	$scope.enter = function(e){
+		$log.log('Ejecutado el evento Mouseenter');
+	};
+	
+	$scope.leave = function(e){
+		$log.log('Ejecutado el evento Mouseleave');
+	};
+	
+	$scope.move = function(e){
+		$scope.pos = 'x: ' + e.x + ' y: ' + e.y;
+	};
+	
+	$scope.over = function(e){
+		$log.log('Ejecutando el evento Mouseover');
+	}
 }]);
