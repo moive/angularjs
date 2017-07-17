2b316ea7f6b8304c52e882a8fd0fa4260564f9d9
moises <mvelasquez@pictoric.com>
2017-07-17 11:04:48 -0500

    controlador en la directiva

---
+++ app/js/Directives/controller-directive.js
@@ -0,0 +1,23 @@
+angular.module('miApp')
+.directive('miDirectiva', ['$log', function($log){
+	return {
+		restrict: 'A',
+		controller: function(){
+			this.log = function(){
+				$log.log('Médoto de la directiva: mi-directiva');
+			};
+		},
+		link: function(scope, element, attr, ctrls){
+			ctrls.log();
+		}
+	};
+}])
+.directive('otraDirectiva', [function(){
+	return {
+		restrict: 'A',
+		require: 'miDirectiva',
+		link: function(scope, element, attr, ctrls){
+			ctrls.log();
+		}
+	};
+}]);
