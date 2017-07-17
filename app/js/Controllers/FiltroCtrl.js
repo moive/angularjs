769f591a0cc0e61fec279e752994e4cee25c26e2
moises <mvelasquez@pictoric.com>
2017-07-17 15:22:42 -0500

    currency

---
+++ app/js/Controllers/FiltroCtrl.js
@@ -0,0 +1,7 @@
+angular.module('miApp')
+.controller('FiltroCtrl', ['$scope', '$filter', function($scope, $filter){
+	var costo =  1453.50;
+	
+	$scope.costo = $filter('currency')(costo);
+	$scope.costoEuro = $filter('currency')(costo, '€', 3);
+}]);
