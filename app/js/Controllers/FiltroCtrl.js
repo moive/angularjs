2da826f01c5ed08947774ed73e0952a1516005ce
moises <mvelasquez@pictoric.com>
2017-07-17 15:33:21 -0500

    limitTo

--- app/js/Controllers/FiltroCtrl.js
+++ app/js/Controllers/FiltroCtrl.js
@@ -5,4 +5,6 @@ angular.module('miApp')
 	$scope.costo = $filter('currency')(costo);
 	$scope.costoEuro = $filter('currency')(costo, '€', 3);
 	
+	 $scope.arreglo = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+	$scope.texto = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Do lorum maxime eum perspiciatis hic corporis sapiente. Ab, provident modi deleniti assumenda nobis ratione, accusantium porro, necessitatibus similique beatae dol oremque perferendis tempora!'; 
 }]);
